f380f672363bcdbb967f34425080ee4e8a30d83e
Shashikant <shashikantprajapti17@gmail.om>
2024-08-13 13:26:03 +0530

    Base URL Changes

--- froutBoxApp/config.js
+++ froutBoxApp/config.js
@@ -3,7 +3,7 @@ export const config = {
   animationColor: '#007FFF', //#5ecb89 green color animation
   APP_ID: 'upsellz_app_001',
   APP_NAME: 'Fruit Box',
-  APP_VERSION: '2.0.1',
+  APP_VERSION: '2.0.0',
   APP_VALIDATOR:
     'W823987erwwebfwiebiusbejwIBBASJBDHJASr18794187923984nfneiwonfrowen**0324newbndjn32x23nx23kbxibwfsljkBSDJKBFJKASBKJAfevtlmeocnnfoiqwoi',
   AFTER_SEND_MESSAGE_BOX: 'Thanks, We recevied your request',
@@ -12,5 +12,6 @@ export const config = {
   WEBSITE_URL: 'https://fruit-box-ya9l.vercel.app', //' http://192.168.43.147:5000' https://upsellz-backend.onrender.com 'https://octopus-app-i5b23.ondigitalocean.app' ,
   // BACKEND_URI: 'http://172.16.2.121:5000', //' http://192.168.43.147:5000' https://upsellz-backend.onrender.com 'https://octopus-app-i5b23.ondigitalocean.app' ,
   // BACKEND_URI: 'https://fruit-box-pied.vercel.app', //' http://192.168.43.147:5000' https://upsellz-backend.onrender.com 'https://octopus-app-i5b23.ondigitalocean.app' ,
-  BACKEND_URI: 'https://squid-app-udvj3.ondigitalocean.app', //' http://192.168.43.147:5000' https://upsellz-backend.onrender.com 'https://octopus-app-i5b23.ondigitalocean.app' ,
+  // BACKEND_URI: 'https://squid-app-udvj3.ondigitalocean.app', //' http://192.168.43.147:5000' https://upsellz-backend.onrender.com 'https://octopus-app-i5b23.ondigitalocean.app' ,
+  BACKEND_URI: 'https://stingray-app-tdjwh.ondigitalocean.app', //' http://192.168.43.147:5000' https://upsellz-backend.onrender.com 'https://octopus-app-i5b23.ondigitalocean.app' ,
 };
